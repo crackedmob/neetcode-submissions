e988349f2b3430cc391829ecfd819f9c3c26f90a
Shipra Sharma <144775532+crackedmob@users.noreply.github.com>
2026-08-13 00:41:32 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int frequency[26] = {0};
+        for(int i = 0; i < s.length(); i++){
+            char c = s[i];
+            frequency[c - 'a']++;
+        }
+        for(int j = 0; j < t.length(); j++){
+            char d = t[j];
+            frequency[d - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(frequency[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
